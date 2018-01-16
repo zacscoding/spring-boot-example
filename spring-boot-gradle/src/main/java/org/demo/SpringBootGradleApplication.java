704ab9cf8027f5d05c9bce06184bc4dfa71b6596
zaccoding <zaccoding725@gmail.com>
2018-01-17 00:06:31 +0900

    spring-boot + gradle test

---
+++ spring-boot-gradle/src/main/java/org/demo/SpringBootGradleApplication.java
@@ -0,0 +1,11 @@
+package org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleApplication.class, args);
+	}
+}
---
+++ spring-boot-gradle/src/main/java/org/demo/domain/Board.java
@@ -0,0 +1,39 @@
+package org.demo.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * @author zerock
+ * @Date 2018-01-16
+ * @GitHub : https://github.com/zerockcode
+ */
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tbl_webboards")
+@EqualsAndHashCode(of = "bno")
+@ToString(exclude="replies")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+
+    private String writer;
+
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp regdate;
+    @UpdateTimestamp
+    private Timestamp updatedate;
+}
