4a6a30f0c1fe2ef1ad87c7df594f2ed5fa3c6ff5
zaccoding <zaccoding725@gmail.com>
2018-02-26 17:59:09 +0900

    ~ ch2.3.1

---
+++ book/spring-boot-inaction/ch02_use_spring_boot/readinglist/src/main/java/readinglist/ReadingListRepository.java
@@ -0,0 +1,14 @@
+package readinglist;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author zaccoding
+ * github : https://github.com/zacscoding
+ */
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByReader(String reader);
+
+}
