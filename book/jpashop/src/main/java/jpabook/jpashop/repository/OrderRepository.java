e7fe9fd3addafc5b78ea703919daf827bf9f043e
zacscoding <zaccoding725@gmail.com>
2019-12-19 21:17:21 +0900

    learning jpa (query dsl)

--- book/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ book/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,17 +1,34 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
-
-import org.springframework.stereotype.Repository;
-
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
-@RequiredArgsConstructor
 @Repository
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        queryFactory = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -20,5 +37,109 @@ public class OrderRepository {
     public Order findOne(Long orderId) {
         return em.find(Order.class, orderId);
     }
-    // public List<Order> findAll(OrderSearch orderSearch) {}
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // return findAllByString(orderSearch);
+        // return findAllByCriteria(orderSearch);
+
+        final QOrder order = QOrder.order;
+        final QMember member = QMember.member;
+
+        return queryFactory.select(order)
+            .from(order)
+            .join(order.member, member)
+            .where(
+                statusEq(orderSearch.getOrderStatus()),
+                nameLike(orderSearch.getMemberName())
+            )
+            .limit(1000)
+            .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCondition) {
+        if (statusCondition == null) {
+            return null;
+        }
+
+        return QOrder.order.status.eq(statusCondition);
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+
+        return QMember.member.name.like(memberName);
+    }
+
+    // jpa criteria
+    // 유지보수성 낮음 => 코드 작성 + 읽기 자체가 어려움
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
+        final CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        final Root<Order> o = cq.from(Order.class);
+        final Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        final List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            criteria.add(cb.equal(o.get("status"), orderSearch.getOrderStatus()));
+        }
+
+        // 멤버 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            criteria.add(cb.like(m.get("name"), '%' + orderSearch.getMemberName() + '%'));
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+
+        return em.createQuery(cq)
+            .setMaxResults(1000)
+            .getResultList();
+    }
+
+    // jpql 문자열 조합
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String qlString = "select o from Order o join o.member m";
+
+        final boolean hasOrderStatus = orderSearch.getOrderStatus() != null;
+        final boolean hasMemberName = StringUtils.hasText(orderSearch.getMemberName());
+
+        boolean isFirstCondition = true;
+
+        if (hasOrderStatus) {
+            if (isFirstCondition) {
+                qlString += " where";
+                isFirstCondition = false;
+            } else {
+                qlString += " and";
+            }
+
+            qlString += " m.status = :status";
+        }
+
+        if (hasMemberName) {
+            if (isFirstCondition) {
+                qlString += " where";
+                isFirstCondition = false;
+            } else {
+                qlString += " and";
+            }
+
+            qlString += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(qlString, Order.class).setMaxResults(1000);
+
+        if (hasOrderStatus) {
+            query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if (hasMemberName) {
+            query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
 }
